dfbbff2711d2d47e590050787f042d3a42ee2ac6
TonyTang1997 <u3528090@connect.hku.hk>
2020-06-03 13:57:27 +0800

    update racecard cleaning item names

--- clean_racecard.py
+++ clean_racecard.py
@@ -11,8 +11,8 @@ df = pd.read_csv('hkrace_cleaned.csv')
 
 racecard = pd.read_json('racecard.json', lines=True)
 
-racecard['race_date_dt'] = pd.to_datetime(racecard['race_date_dt'].astype(str), format='%Y%m%d')
-racecard['race_date_dt'] = racecard['race_date_dt'].apply(lambda x: str(x).split(" ")[0])
+racecard['race_date'] = pd.to_datetime(racecard['race_date'].astype(str), format='%Y%m%d')
+racecard['race_date'] = racecard['race_date'].apply(lambda x: str(x).split(" ")[0])
 
 racecard['result'] = np.nan
 racecard['LBW'] = np.nan
@@ -23,7 +23,7 @@ venue_dict = {"HappyValley":"HV","ShaTin":"ST"}
 racecard['venue'] = racecard['venue'].map(venue_dict)
 
 racecard['track'] = racecard['track'].str.upper()
-racecard['going'] = racecard['going'].str.upper()
+racecard['config'] = racecard['config'].str.upper()
 
 racecard['race_no'] = racecard['race_no'].apply(lambda x:int(x))
 racecard['distance'] = racecard['distance'].apply(lambda x:int(x))
@@ -35,7 +35,7 @@ racecard = racecard.reset_index(drop=True)
 
 combined_df = pd.concat([df,racecard])
 
-combined_df = combined_df.sort_values(['race_date_dt','race_no','finish_time','horse_number'],ascending=[True,True,True,True])
+combined_df = combined_df.sort_values(['race_date','race_no','finish_time','horse_number'],ascending=[True,True,True,True])
 
 combined_df = combined_df[df.columns]
 
