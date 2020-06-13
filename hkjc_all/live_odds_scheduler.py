9b9167d1a4c909d4c9c7184843a5ec5ba987c06f
TonyTang1997 <u3528090@connect.hku.hk>
2020-06-14 00:42:01 +0800

    while style recursive crawling

--- hkjc_all/live_odds_scheduler.py
+++ hkjc_all/live_odds_scheduler.py
@@ -9,18 +9,17 @@ from hkjc_all.spiders.live_investment import LiveInvestmentSpider
 from hkjc_all.spiders.live_qin import LiveQinSpider
 from hkjc_all.spiders.live_qpl import LiveQplSpider
 
-def crawl_and_export():
-    settings = get_project_settings()
-    runner = CrawlerRunner(settings)
-    runner.crawl(LiveWinOddsSpider)
-    runner.crawl(LiveInvestmentSpider)
-    runner.crawl(LiveQinSpider)
-    runner.crawl(LiveQplSpider)
+@defer.inlineCallbacks
+def crawl():
 
-    d = runner.join()
-    d.addBoth(lambda _: reactor.stop())
+    yield runner.crawl(LiveWinOddsSpider)
+    yield runner.crawl(LiveInvestmentSpider)
+    yield runner.crawl(LiveQinSpider)
+    yield runner.crawl(LiveQplSpider)
 
-    reactor.run() # the script will block here until all crawling jobs are finished    
+    reactor.stop()
+
+def export_to_bucket():
 
     os.system('mongoexport --db hkjc --collection live_winodds --out live_winodds.json')
     os.system('mongoexport --db hkjc --collection live_investment --out live_investment.json')
@@ -33,6 +32,12 @@ def crawl_and_export():
 
 if __name__=="__main__":
     while True:
-        crawl_and_export()
+        settings = get_project_settings()
+        runner = CrawlerRunner(settings)
+
+        crawl()
+        reactor.run() # the script will block here until the last crawl call is finished
+        export_to_bucket()
+
         for i in range(5):
             time.sleep(1)
