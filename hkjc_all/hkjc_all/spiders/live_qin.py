b962befd3770b3a84eeb02252db5f940ac0ed20f
TonyTang1997 <u3528090@connect.hku.hk>
2020-06-13 20:58:37 +0800

    live qin and qpl

--- hkjc_all/hkjc_all/spiders/live_qin.py
+++ hkjc_all/hkjc_all/spiders/live_qin.py
@@ -13,7 +13,7 @@ from selenium import webdriver
 import pandas as pd
 from selenium.webdriver.chrome.options import Options
 from datetime import datetime, timedelta
-from hkjc_all.items import HkjcLiveInvestmentItem
+from hkjc_all.items import HkjcLiveQinItem
 from scrapy import Request
 from requests_html import HTMLSession, AsyncHTMLSession
 import urllib.request, json 
@@ -32,7 +32,7 @@ racedays = pd.read_csv(racedays_dir)
 racedays = racedays.sort_values("date").reset_index(drop=True)
 
 racedays['date'] = pd.to_datetime(racedays['date'], format='%Y/%m/%d')
-race_before_today = racedays[racedays.date < datetime.now()].reset_index(drop=True)
+race_before_today = racedays[racedays.date < (datetime.now() + timedelta(hours=8))].reset_index(drop=True)
 
 next_raceday = racedays['date'][len(race_before_today)]
 next_race_venue = racedays['venue'][len(race_before_today)]
@@ -44,16 +44,15 @@ except:
     all_race_no = 0
     pass
 
-class LiveInvestmentSpider(scrapy.Spider):
+class LiveQinSpider(scrapy.Spider):
 
-    name = "live_investment"
+    name = "live_qin"
     
-    custom_settings = {'ITEM_PIPELINES': {'hkjc_all.pipelines.MongoDBLiveInvestment': 400}}
+    custom_settings = {'ITEM_PIPELINES': {'hkjc_all.pipelines.MongoDBLiveQin': 400}}
 
-    start_urls = []
-    for i in range(all_race_no):
-        tmp_url = "http://bet.hkjc.com/racing/getJSON.aspx/?type=pooltot&date={}&venue={}&raceno={}".format(next_raceday.strftime('%Y-%m-%d'),next_race_venue,i+1)
-        start_urls.append(tmp_url)
+    start_urls = [] 
+    tmp_url = "https://bet.hkjc.com/racing/getJSON.aspx/?type=qin&date={}&venue={}&raceno={}".format(next_raceday.strftime('%Y-%m-%d'),next_race_venue,all_race_no)
+    start_urls.append(tmp_url)
 
     def __init__(self):
         chrome_options = Options()
@@ -61,35 +60,37 @@ class LiveInvestmentSpider(scrapy.Spider):
         chrome_options.add_argument("--no-sandbox")
         chrome_options.add_argument("--disable-dev-shm-usage")
         self.browser = webdriver.Chrome("/usr/bin/chromedriver", chrome_options=chrome_options)
-
+        
         print(next_raceday)
         print(next_race_venue)
 
     def parse(self, response):
 
-        main = HkjcLiveInvestmentItem()
+        main = HkjcLiveQinItem()
 
         session = HTMLSession()
         r = session.get(response.url)
         data = json.loads(r.html.html)
 
+        time, odds = data['OUT'].split('@@@')[0], data['OUT'].split('@@@')[1:]
+
         main["time_scaped"] = (datetime.now() + timedelta(hours=8))
-        main["time_updated_by_hkjc"] = str((datetime.now() + timedelta(hours=8)).strftime('%Y-%m-%d')) + "-" + str(data['updateTime'])
+        main["time_updated_by_hkjc"] = str((datetime.now() + timedelta(hours=8)).strftime('%Y-%m-%d')) + "-" + str(time)
         main["race_date"] = next_raceday
         main["venue"] = next_race_venue
         main['race_no'] = str(response.url).split('=')[-1]
-        main['total_investment'] = data['totalInv']
 
-        investment_value = [x['value'] for x in data['inv']]
-        
-        main['win_investment'] = investment_value[0]
-        main['pla_investment'] = investment_value[1]
-        main['qin_investment'] = investment_value[2]
-        main['qpl_investment'] = investment_value[3]
-        main['tce_investment'] = investment_value[4]
-        main['tri_investment'] = investment_value[5]
-        main['qtt_investment'] = investment_value[7]
-        main['dbl_investment'] = investment_value[8]
-        
+        qin_odds_list = odds[0].split(';')[1:]
+        qin_odds = list(map(lambda x: x.split('=')[1], qin_odds_list))
+
+        combs_dict = {91:14,78:13,66:12,55:11,45:10,36:9,28:8,21:7,15:6,10:5,6:4,3:3}
+
+        counter  = 0
+
+        for i in range(combs_dict[len(qin_odds)]):
+            for j in range(i+1):
+                main['qin_'+str(i+1)+'_'+str(j+1)] = wo[counter]
+                counter += 1
+
         yield main
 
