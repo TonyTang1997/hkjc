88c9d6e298ef5125b8370b15b839426bf37946db
TonyTang1997 <u3528090@connect.hku.hk>
2020-10-25 13:21:04 +0800

    correct year

--- hkjc_all/hkjc_all/spiders/racedays.py
+++ hkjc_all/hkjc_all/spiders/racedays.py
@@ -27,7 +27,7 @@ class RaceDaySpider(scrapy.Spider):\
     }
     
     start_urls = [] 
-    for i in range(1979,str(datetime.now().year)):
+    for i in range(1979,int(datetime.now().year)):
         for j in ['01','02','03','04','05','06','07','09','10','11','12']:  #never have races in Aug
             tmp_urls = "https://racing.hkjc.com/racing/information/English/Racing/Fixture.aspx/?CalYear={}&CalMonth={}".format(i,j) #"/" before aspx is important to escape challenge
             start_urls.append(tmp_urls)
